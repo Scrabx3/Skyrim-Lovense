784510428e28b600c0903e33522dceb469b59051
Scrabx3 <kris.vlntn@gmail.com>
2024-10-23 21:45:13 +0200

    Request subclasses

--- src/Lovense/Request.h
+++ src/Lovense/Request.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include "Action.h"
+#include "Preset.h"
+
 namespace Lovense
 {
+	constexpr static inline size_t MAX_PATTERN_STRENGTH_COUNT = 50;
+
 	struct Request
 	{
 		friend class RequestHandler;
@@ -31,5 +36,82 @@ namespace Lovense
 		std::optional<json> result{};
 		std::optional<std::string> error{};
 	};
+
+	struct GetToys_Request : public Request
+	{
+		GetToys_Request() :
+			Request(true) {}
+		~GetToys_Request() = default;
+		std::string GetCommand() const override;
+	};
+
+	struct GetToyName_Request : public Request
+	{
+		GetToyName_Request() :
+			Request(true) {}
+		~GetToyName_Request() = default;
+		std::string GetCommand() const override;
+	};
+
+	struct Function_Request : public Request
+	{
+		Function_Request(std::vector<Action> action, std::vector<int> strength, double timeSec,
+				std::optional<double> loopRunningSec, std::optional<double> loopPauseSec, std::optional<std::string> toy, std::optional<int> stopPrevious);
+		~Function_Request() = default;
+		std::string GetCommand() const override;
+
+	private:
+		std::vector<Action> action;
+		std::vector<int> strength;
+		double timeSec;
+		std::optional<double> loopRunningSec;
+		std::optional<double> loopPauseSec;
+		std::optional<std::string> toy;
+		std::optional<int> stopPrevious;
+		static inline constexpr int apiVer = 1;
+	};
+
+	struct Position_Request : public Request
+	{
+		Position_Request(int value, std::optional<std::string> toy = std::nullopt) :
+			Request(false), value(std::to_string(std::clamp(value, 0, 100))), toy(toy) {};
+		~Position_Request() = default;
+		std::string GetCommand() const override;
+
+	private:
+		std::string value;
+		std::optional<std::string> toy;
+		static inline constexpr int apiVer = 1;
+	};
+
+	struct Pattern_Request : public Request
+	{
+		Pattern_Request(const std::vector<Action>& actions, const std::vector<int>& strengths, int intervalMs, double timeSec,
+				std::optional<std::string> toy = std::nullopt);
+		~Pattern_Request() = default;
+		std::string GetCommand() const override;
+
+	private:
+	  std::vector<Action> actions;
+		std::vector<int> strengths;
+		int intervalMs;
+		double timeSec;
+		std::optional<std::string> toy;
+		static inline constexpr int apiVer = 2;
+	};
+
+ struct Preset_Request : public Request
+ {
+	 Preset_Request(Preset preset, double timeSec, std::optional<std::string> toy = std::nullopt) :
+		 Request(false), preset(preset), timeSec(timeSec), toy(toy) {};
+	 ~Preset_Request() = default;
+	 std::string GetCommand() const override;
+
+	 private:
+		 Preset preset;
+		 double timeSec;
+		 std::optional<std::string> toy;
+		 static inline constexpr int apiVer = 1;
+ };
 
 }	 // namespace Lovense
